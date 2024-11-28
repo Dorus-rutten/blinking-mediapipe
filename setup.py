a3791010f48560d6844a83e025b11b39f953408b
Dorus-Rutten <dorusrutten@gmail.com>
2024-11-28 14:00:59 +0100

    Added a setup.py file to the project

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="blinking-mediapipe",
+    version="0.1.0",
+    author="Dorus Rutten",
+    author_email="Dorsrutten@gmail.com",
+    description="A Mediapipe-based project to track blinking and analyze eye movements.",
+    url="https://github.com/Dorus-rutten/Rock-Paper-Scissers",
+    packages=find_packages(),
+    install_requires=[
+        "opencv-contrib-python==4.8.0.74",
+        "opencv-python==4.8.0.74",
+        "mediapipe==0.10.14",
+        "numpy==1.25.1"
+    ],
+    python_requires='==3.9.20',
+)
